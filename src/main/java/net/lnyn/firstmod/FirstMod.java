d4b703ce654b44371020fa1035638e008fcf37fe
HP <riverfrontrm@hotmail.com>
2025-12-16 16:19:30 -0800

    a

--- src/main/java/net/lnyn/firstmod/FirstMod.java
+++ src/main/java/net/lnyn/firstmod/FirstMod.java
@@ -8,7 +8,7 @@ import org.slf4j.LoggerFactory;
 public class FirstMod implements ModInitializer {
 	public static final String MOD_ID = "first-mod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
+//hi
 	@Override
 	public void onInitialize() {
 
