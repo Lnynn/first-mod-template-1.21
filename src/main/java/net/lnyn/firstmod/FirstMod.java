89f0f7ad32049d1b00736d9d2910a74c86268cd2
HP <riverfrontrm@hotmail.com>
2025-12-16 16:18:56 -0800

    first commit

---
+++ src/main/java/net/lnyn/firstmod/FirstMod.java
@@ -0,0 +1,16 @@
+package net.lnyn.firstmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FirstMod implements ModInitializer {
+	public static final String MOD_ID = "first-mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
